3aa5e8ce0a0dceab4f786afdc450117f45690131
Simon <simon.git@siraj.eu>
2022-02-11 18:08:25 +0000

    Attempt 1 to convert to Text Only

--- facemash.py
+++ facemash.py
@@ -6,59 +6,68 @@ import configparser
 import random
 import os
 
+from facemash import Subjects
+
 app = Flask(__name__)
 app.config.from_pyfile('config.py.sample')
 db = SQLAlchemy(app)
 
-class Persons(db.Model):
+
+class Subjects(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    path = db.Column(db.String(), unique=True, nullable=False)
+    name = db.Column(db.String(), unique=True, nullable=False)
     upvotes = db.Column(db.Integer(), nullable=False, default=0)
     downvotes = db.Column(db.Integer(), nullable=False, default=0)
     elo_rank = db.Column(db.Float(), nullable=False, default=1200)
 
-    def __init__(self, path: str):
+    def __init__(self, name: str):
         self.upvotes = 0
         self.downvotes = 0
         self.elo_rank = 1200
-        self.path = path
+        self.name = name
 
     def __repr__(self):
-        return '<Person %r>' % self.id
+        return '<Subject %r>' % self.id
+
+
 K = 32
+
 
 @app.route('/')
 def home() -> render_template:
     """ Try to serve the main view if it fails due to ValueError catch it and serve a Too few contestants page instead
         Else show a generic error template with the exception printed out."""
     try:
-        return render_template('index.html', contestants = random.sample(Persons.query.all(), 2))
+        return render_template('index.html', contestants=random.sample(Subjects.query.all(), 2))
     except ValueError:
-        return render_template('error.html', exception="Too few contestants in database to bee able to start the game.")
+        return render_template('error.html', exception="Too few contestants in database to be able to start the game.")
     except Exception as excp:
         return render_template('error.html', exception=excp)
 
+
 @app.route('/toplist')
 def toplist() -> render_template:
-    all_persons = Persons.query.order_by(Persons.elo_rank.desc()).all()
-    if len(all_persons) > 1:
-        return render_template('toplist.html', persons=all_persons)
+    all_subjects = Subjects.query.order_by(Subjects.elo_rank.desc()).all()
+    if len(all_subjects) > 1:
+        return render_template('toplist.html', subjects=all_subjects)
     else:
-        return render_template('error.html', exception="Not enough contestants to display the toplist")
+        return render_template('error.html', exception="Not enough items to display the toplist")
+
 
 @app.route('/upload')
 def upload() -> render_template:
     """ Serve the upload page statically, this should not be able to fail. No try/catch needed for now."""
     return render_template('upload.html')
 
+
 @app.route('/vote/<int:winner>/<int:loser>')
 def vote(winner: int, loser: int) -> redirect:
     """ The function for the vote, modify the users elo_rank by the correct formulas.
         :returns a redirect"""
-    # Fetch the winner and the looser from the database from the database by ID.
-    winner: Persons = Persons.query.filter_by(id=winner).first()
-    loser: Persons = Persons.query.filter_by(id=loser).first()
-    
+    # Fetch the winner and the loser from the database from the database by ID.
+    winner: Subjects = Subjects.query.filter_by(id=winner).first()
+    loser: Subjects = Subjects.query.filter_by(id=loser).first()
+
     winner.upvotes += 1
     loser.downvotes += 1
 
@@ -71,32 +80,17 @@ def vote(winner: int, loser: int) -> redirect:
     db.session.commit()
     return redirect(url_for('home'))
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_FORMATS']
 
-@app.route('/upload_image', methods=['GET', 'POST'])
+@app.route('/add_item', methods=['GET', 'POST'])
 def upload_file() -> redirect:
     """Upload files to static/images and return a redirect """
     if request.method == 'POST':
-        # if there is no file in the request throw an error
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files['file']
-        # If the user does not select a file, the browser submits an empty file without a filename.
-        if file.filename == '':
-            return redirect(request.url)
-        # If a file is sent convert it to a secure filename
-        # TODO: Set it as a UUID16 or something
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            location = os.path.join(app.config['IMAGE_DIR'], filename)
-            file.save(location)
+        # Insert the image in the database.
+        Project = request.form.get("Project")
+        subject = Subjects(name=Project)
+        db.session.add(subject)
+        db.session.commit()
 
-            # Insert the image in the database.
-            person = Persons(path=location)
-            db.session.add(person)
-            db.session.commit()
-
-            return redirect(url_for('home', name=filename))
-
+        return redirect(url_for('home'))
+    else:
+        return render_template('error.html', exception="Cannot access this page directly")
